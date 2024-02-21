c30f94781c6bafde95b626c13e692ae3218250c3
Patryk Prusko <patrykprusko@gmail.com>
2024-02-21 02:06:37 +0100

    Added a new algorithm for generating new match results, which are then added to match queues, the class -> 'GenerateMatchResults' is responsible for this. In addition, the goals are from 0 to 5. Winning 1 team -> 15 points, winning 2 team -> 30 points, for an away draw -> 3 points. Then it sorts by Points value, then when two teams have equal number of points, the order -> 'MeetingsWon' decides.

--- WebAppMVC.Infrastructure/Seeders/FootballTeamSeeder.cs
+++ WebAppMVC.Infrastructure/Seeders/FootballTeamSeeder.cs
@@ -8,8 +8,6 @@ namespace WebAppMVC.Infrastructure.Seeders
     {
         private readonly WebAppMVCDbContext _dbContext;
         private string[] listsTeamsFirstLeagua = new string[16] { "Genk", "Royale Union SG", "Antwerp", "Club Brugge", "Gent", "St. Liege", "Westerlo", "Cercle Brugge", "Charleroi", "Leuven", "Anderlecht", "St. Truiden", "KV Mechelen", "Kortrijk", "Eupen", "Oostende" };
-        private string[] listsTeamsSecondLeagua = new string[6] { "Zulte-Waregem", "Seraing", "KFCO Beerschot-Wilrijk", "Deinze", "Lommel", "Patro Eisden", };
-        private string[] listsQueues = new string[15] { "First", "Second", "Third", "Fourth", "Fifth", "Sixth","Seventh" , "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth" }; 
         public FootballTeamSeeder(WebAppMVCDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -52,58 +50,10 @@ namespace WebAppMVC.Infrastructure.Seeders
 
         public IEnumerable<Queue> GetQueues(string leagueName)
         {
-            var footballTeams = _dbContext.FootballTeams.Where(t => t.LeagueName == leagueName).ToList(); // Belgian League
+            IGenerateMatchResults generateMatchResults = new GenerateMatchResults(_dbContext);
 
-            var matches = new List<Match>();
-            var queries = new List<Queue>();
+            return generateMatchResults.GenerateMatchResultGetMatchResults(leagueName);
 
-            int numberOfTeams = 16;
-
-            // Initialisation of the teams
-            int[] teams = new int[numberOfTeams];
-            for (int i = 0; i < numberOfTeams; i++)
-            {
-                teams[i] = i + 1;
-
-                if (i == 15) break;
-                queries.Add(new Queue() { Description = listsQueues[i], Matches = new List<Match>() });
-
-            }
-
-            // Start of the tournament
-            for (int round = 1; round <= numberOfTeams - 1; round++)
-            {
-
-                // Transfer of teams
-                int temp = teams[numberOfTeams - 1];
-                Array.Copy(teams, 0, teams, 1, numberOfTeams - 1);
-                teams[0] = temp;
-
-                // Generation of match pairs
-                for (int i = 0; i < numberOfTeams / 2; i++)
-                {
-                    var firstMatch = teams[i];
-                    var secondMatch = teams[i + numberOfTeams / 2];
-                    Random random = new Random();
-                    var match = new Match() { NameFirstTeam = footballTeams[firstMatch - 1].Name, NameSecondTeam = footballTeams[secondMatch - 1].Name, Result = random.Next(2)+1 };
-                    match.GoalScore = match.Result == 1 ? "1 : 0" : "0 : 1";
-                    match.QueueName = queries[round - 1].Description;
-                    match.LeagueId = footballTeams[round - 1].LeagueId;
-                    queries[round - 1].Matches.Add(match);
-
-                    var footballTeam = footballTeams.FirstOrDefault(f => f.Name == (match.Result == 1 ? match.NameFirstTeam : match.NameSecondTeam));
-                    if (footballTeam != null)
-                    {
-                        footballTeam.MeetingsWon++;
-                        footballTeam.Points += 10;
-                        _dbContext.FootballTeams.Update(footballTeam);
-                    }
-                    
-                    _dbContext.SaveChanges();
-                }
-
-            }
-            return queries;
         }
 
     }
---
+++ WebAppMVC.Infrastructure/Seeders/GenerateMatchResults.cs
@@ -0,0 +1,142 @@
+﻿
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WebAppMVC.Domain.Entities;
+using WebAppMVC.Infrastructure.Persistence;
+
+namespace WebAppMVC.Infrastructure.Seeders
+{
+    interface IGenerateMatchResults
+    {
+        public IEnumerable<Queue> GenerateMatchResultGetMatchResults(string leagueName);
+    }
+    public class GenerateMatchResults : IGenerateMatchResults
+    {
+        private readonly WebAppMVCDbContext _dbContext;
+        private string[] ListBelgianFirstLeagueTeams = new string[16] { "Genk", "Royale Union SG", "Antwerp", "Club Brugge", "Gent", "St. Liege", "Westerlo", "Cercle Brugge", "Charleroi", "Leuven", "Anderlecht", "St. Truiden", "KV Mechelen", "Kortrijk", "Eupen", "Oostende" };
+        private string[] listBelgianSecondLeaguaTeams = new string[6] { "Zulte-Waregem", "Seraing", "KFCO Beerschot-Wilrijk", "Deinze", "Lommel", "Patro Eisden", };
+        private string[] listsQueues = new string[15] { "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth" };
+
+        public GenerateMatchResults(WebAppMVCDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+         IEnumerable<Queue> IGenerateMatchResults.GenerateMatchResultGetMatchResults(string leagueName)
+        {
+            var footballTeams = _dbContext.FootballTeams.Where(t => t.LeagueName == leagueName).ToList(); 
+
+            string[] firstLEaqueTeams = new string[16];
+
+            if (leagueName.Equals("Belgian"))
+            {
+                var results = ListBelgianFirstLeagueTeams.ToArray();
+                Array.Copy(results, firstLEaqueTeams, results.Length);
+            }
+            else
+            {
+                for (int i = 0; i < firstLEaqueTeams.Length; i++)
+                {
+                    firstLEaqueTeams[i] = footballTeams[i].Name;
+                }
+            }
+
+            var matches = new List<Domain.Entities.Match>();
+            var queries = new List<Queue>();
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (i == 15) break;
+                queries.Add(new Queue() { Description = listsQueues[i], Matches = new List<Domain.Entities.Match>() });
+            }
+
+            int rounds = firstLEaqueTeams.Length - 1;
+
+            
+            for (int round = 1; round <= rounds; round++)
+            {
+                // Console.WriteLine($"Queue {round}:");
+
+                for (int i = 0; i < firstLEaqueTeams.Length / 2; i++)
+                {
+                    int team1Index = i;
+                    int team2Index = firstLEaqueTeams.Length - 1 - i;
+
+                    int team1Score = GenerateRandomScore();
+                    int team2Score = GenerateRandomScore();
+
+                    // Console.WriteLine($"{listsTeamsFirstLeagua[team1Index]} vs {listsTeamsFirstLeagua[team2Index]}  results: {team1Score} : {team2Score}");
+
+                    var newMatch = CreateMatch(firstLEaqueTeams[team1Index], firstLEaqueTeams[team2Index], team1Score, team2Score);
+                   
+                    newMatch.QueueName = queries[round - 1].Description;
+                    newMatch.LeagueId = footballTeams[round - 1].LeagueId;
+                    queries[round - 1].Matches.Add(newMatch);
+
+                    string searchedTeam = "";
+                    int points = 0;
+                    switch(newMatch.Result)
+                    {
+                        case 1:
+                            searchedTeam = newMatch.NameFirstTeam;
+                            points = 15;
+                            break;
+                        case 2:
+                            searchedTeam = newMatch.NameSecondTeam;
+                            points = 30;
+                            break;
+                        default:
+                            searchedTeam = newMatch.NameSecondTeam;
+                            points = 3;
+                            break;
+
+                    }
+
+                    var footballTeam = footballTeams.FirstOrDefault(f => f.Name == searchedTeam);
+                    if (footballTeam != null)
+                    {
+                        footballTeam.MeetingsWon++;
+                        footballTeam.Points += points;
+                        _dbContext.FootballTeams.Update(footballTeam);
+                    }
+
+                    _dbContext.SaveChanges();
+                }
+
+                RotateTeams(firstLEaqueTeams); 
+            }
+
+            return queries;
+        }
+
+        private Domain.Entities.Match CreateMatch(string nameFirstName, string nameSecondName, int team1Score, int team2Score)
+        {
+            Domain.Entities.Match match = new Domain.Entities.Match() { NameFirstTeam = nameFirstName, NameSecondTeam = nameSecondName };
+            if (team1Score > team2Score) match.Result = 1; //first team win
+            else if (team1Score < team2Score) match.Result = 2; // second team win
+            else match.Result = 0; // draw in match
+
+            match.GoalScore = $"{team1Score} : {team2Score}";
+            return match;
+        }
+
+        private void RotateTeams(string[] teams)
+        {
+            // Move the teams one position to the left (except for the first team)
+            string firstTeam = teams[0];
+
+            for (int i = 1; i < teams.Length; i++)
+            {
+                teams[i - 1] = teams[i];
+            }
+
+            teams[teams.Length - 1] = firstTeam;
+        }
+
+        private int GenerateRandomScore()
+        {
+            Random random = new Random();
+            return random.Next(0, 6); // draw in match for simplicity, we assume that the result can be from 0 to 5.
+        }
+    }
+}
